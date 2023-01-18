9063576b6d6f9a332a5bbe112a206d7886d6406f
kellylayn <kellyrabbit1@gmail.com>
2023-01-18 07:00:43 +0000

    [rest]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    # @book = Book.find(params[:id])
+  end
+
+  def edit
+    # @book = Book.find(params[:id])
+  end
+
+  def apdate
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
