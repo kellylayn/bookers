d5e9accbb6ef0b542e35b635c12bca9772e0a1cb
kellylayn <kellyrabbit1@gmail.com>
2023-01-19 12:57:50 +0000

    [finish?]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      # @book = Book.new
+      @books = Book.all
       render :index
     end
   end
@@ -20,20 +22,23 @@ class BooksController < ApplicationController
   end
 
   def edit
-    # @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
-  def apdate
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully update."
-    redirect_to book_path(book.id)
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "Book was successfully destroy."
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
